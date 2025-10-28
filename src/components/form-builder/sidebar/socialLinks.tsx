cf41b1caaf7b5ec9a914fbd4176a82648f2307c2
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-10-28 11:55:32 +0300

    update social links and remove unused icons

--- src/components/form-builder/sidebar/socialLinks.tsx
+++ src/components/form-builder/sidebar/socialLinks.tsx
@@ -1,5 +1,4 @@
-import { SiBuymeacoffee } from "@icons-pack/react-simple-icons";
-import { FaLinkedin, FaProductHunt } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
 
 import { FaGithub } from "react-icons/fa6";
 
@@ -9,30 +8,16 @@ export default function SocialLinks() {
     <a
       className="flex items-center gap-2 text-sm hover:text-slate-500"
       target="_blank"
-      href="https://github.com/iduspara/shadcn-builder"
+      href="https://github.com/youneedgreg/Compono"
     >
       <FaGithub className="w-6 h-6" />
     </a>
     <a
       className="flex items-center gap-2 text-sm hover:text-slate-500"
       target="_blank"
-      href="https://www.linkedin.com/in/igor-duspara-b97aa1300/"
+      href="https://www.linkedin.com/in/youneedgreg/"
     >
       <FaLinkedin className="w-6 h-6" />
-    </a>
-    <a
-      className="flex items-center gap-2 text-sm hover:text-slate-500"
-      target="_blank"
-      href="https://buymeacoffee.com/igorduspara"
-    >
-      <SiBuymeacoffee className="w-6 h-6" />
-    </a>
-    <a
-      className="flex items-center gap-2 text-sm hover:text-slate-500"
-      target="_blank"
-      href="https://www.producthunt.com/@iduspara"
-    >
-      <FaProductHunt className="w-6 h-6" />
     </a>
   </div>
   );
